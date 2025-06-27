d6af92eb219582b038b15efe3a082d5bb417172e
Christian Nunciato <chris@nunciato.org>
2025-06-27 16:55:25 -0700

    Vitest

---
+++ app/vitest.config.js
@@ -0,0 +1,11 @@
+export default defineConfig({
+    test: {
+        // Send results to Test Engine
+        reporters: [
+            'default',
+            'buildkite-test-collector/vitest/reporter'
+        ],
+        // Enable column + line capture for Test Engine
+        includeTaskLocation: true,
+    }
+})
